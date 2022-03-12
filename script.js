630336ac250bb1777e1583b4eb2aea8aff8f2f5e
DanJak010 <daniele.martini2705@gmail.com>
2022-03-12 23:51:59 +0100

    agg2.0

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function openUrl(url, pagina) {
     window.open(url, pagina)
 }
+
+
+
+function copia(testo) {
+    var input = document.createElement('input');
+    var copia = document.getElementById(testo).value;
+    input.setAttribute('value', copia);
+    document.body.appendChild(input);
+    input.select();
+    var risultato = document.execCommand('copy');
+    document.body.removeChild(input);
+    alert('IL TESTO È STATO COPIATO CON SUCCESSO!');
+    return risultato;
+}
+
